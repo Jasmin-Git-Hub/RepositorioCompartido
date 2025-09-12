b7d82ec75354527d67ce10d6a36803126e8f575a
Jasmin Alcántara <205731838+Jasmin-Git-Hub@users.noreply.github.com>
2025-09-12 15:23:59 -0500

    Reṕositorio compartido

---
+++ sobrecarga/src/Controlador.java
@@ -0,0 +1,51 @@
+
+import java.io.IOException;
+import java.util.Scanner;
+public class Controlador {
+    private final Scanner sc;
+    
+    public Controlador(Scanner sc){
+        this.sc = sc;
+    }
+    public void menu() throws IOException{
+        int opc;
+        do{
+            System.out.println("\nMenú de opciones");
+            System.out.println("1. Calculadora");
+            System.out.println("2. Conversor");
+            System.out.println("3. Contador");
+            System.out.println("4. Banco");
+            System.out.println("5. Auto");
+            System.out.println("6. Libro");
+            System.out.println("7. División");
+            System.out.println("8. División con"
+                    + " entrada de usuario");
+            System.out.println("9. ValidarEdad");
+            System.out.println("10. Archivo");
+            System.out.println("0. Salir");
+            System.out.print("Elija una opción:");
+            try {
+                opc = Integer.parseInt(sc.nextLine());
+            } catch (NumberFormatException e) {
+                opc = -1;
+            }
+            switch (opc) {
+                case 1:
+                    demoCalculadora();
+                    break;                
+            }
+        }
+        while(true && !false);
+        
+    }
+   private void demoCalculadora(){
+       Calculadora c = new Calculadora();
+       System.out.println("Sumar 2 + 3 = " +
+               c.sumar(2, 3));
+       System.out.println("Sumar 1+2+3=" + 
+               c.sumar(1,2, 3));
+       System.out.println("Sumar 2.3 + 5.9 =" +
+               c.sumar(2.3, 5.9));
+   } 
+    
+}
---
+++ sobrecarga/src/Principal.java
@@ -0,0 +1,23 @@
+
+import java.io.IOException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+public class Principal {
+
+    public static void main(String[] args) {
+       Controlador controlador = new Controlador(new Scanner(System.in));
+        try {
+            controlador.menu();
+        } catch (IOException e) {
+            System.out.println("Ocurrió un error de E/S: " 
+                    + e.getMessage());
+        } catch (InputMismatchException e){
+            System.out.println("Entrada de no válida "+
+                    e.getMessage());
+        } catch (Exception e){
+            System.out.println("Error no controlado " + 
+                    e.getMessage());
+        }
+    }
+    
+}
